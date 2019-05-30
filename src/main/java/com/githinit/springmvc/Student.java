4c8d6b727845cf3c029d96115b456321d4ce4d5b
geoffrey.mwaura <geoffrey.mwaura@openworld.co.ke>
2019-05-30 16:21:17 +0300

    drop down lists

--- src/main/java/com/githinit/springmvc/Student.java
+++ src/main/java/com/githinit/springmvc/Student.java
@@ -1,12 +1,37 @@
 package com.githinit.springmvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
     private String firstName;
 
     private String lastName;
 
+    private String country;
+
+    private LinkedHashMap<String, String> countryOptions;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+
     public Student() {
+
+        //populate country options
+
+        countryOptions = new LinkedHashMap<>();
+
+        countryOptions.put("BR", "Brazil");
+        countryOptions.put("FR", "France");
+        countryOptions.put("KE", "Kenya");
+        countryOptions.put("DE", "Germany");
+
     }
 
     public String getFirstName() {
@@ -23,5 +48,10 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+
+    public LinkedHashMap<String, String> getCountryOptions() {
+        return countryOptions;
     }
 }
