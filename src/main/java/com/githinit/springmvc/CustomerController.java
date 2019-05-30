2b642306dc80e12e449131eda764a280f6ef9e1c
geoffrey.mwaura <geoffrey.mwaura@openworld.co.ke>
2019-05-30 18:24:33 +0300

    validating min and max field values

---
+++ src/main/java/com/githinit/springmvc/CustomerController.java
@@ -0,0 +1,49 @@
+package com.githinit.springmvc;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+    // add initbinder to trim strings
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+    @GetMapping("/showForm")
+    public String showForm(Model model) {
+
+        // add student object to the model
+        model.addAttribute("customer", new Customer());
+
+        return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+
+        System.out.println("Last name: |" + customer.getLastName() + "|");
+
+
+        if (bindingResult.hasErrors()) {
+            return "customer-form";
+        }
+
+        //log input data
+        System.out.println("customer: " + customer.getFirstName() + " " + customer.getLastName());
+
+
+        return "customer-confirmation";
+    }
+}
