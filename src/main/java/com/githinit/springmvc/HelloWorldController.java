5666d5161f65a10fa91befdf2cd1789f895d7f86
geoffrey.mwaura <geoffrey.mwaura@openworld.co.ke>
2019-05-30 14:39:21 +0300

    controller level request mapping

--- src/main/java/com/githinit/springmvc/HelloWorldController.java
+++ src/main/java/com/githinit/springmvc/HelloWorldController.java
@@ -3,9 +3,11 @@ package com.githinit.springmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
     // show form
 
@@ -17,8 +19,16 @@ public class HelloWorldController {
     // process form
     @GetMapping("/processForm")
     public String processForm(@RequestParam(name = "studentName", required = false) String name, Model model) {
-        model.addAttribute("studentName",  name.toUpperCase());
+        model.addAttribute("studentName", name.toUpperCase());
         return "helloworld";
     }
 
+
+    @GetMapping("/processFormVersionTwo")
+    public String letsShoutDude(@RequestParam(name = "studentName", required = false) String name, Model model) {
+        model.addAttribute("message", "Yo!" + name.toUpperCase());
+
+
+        return "helloworld";
+    }
 }
