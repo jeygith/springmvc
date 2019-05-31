cf3b8ec9b12199b47ff0c549371f32ca45a5d188
geoffrey.mwaura <geoffrey.mwaura@openworld.co.ke>
2019-05-31 18:25:51 +0300

    cascade delete for instructor detail

---
+++ src/main/java/com/onetoone/hibernate/GetInstructorDetailDemo.java
@@ -0,0 +1,48 @@
+package com.onetoone.hibernate;
+
+import com.onetoone.hibernate.entity.Instructor;
+import com.onetoone.hibernate.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetInstructorDetailDemo {
+
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate2.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+
+        try {
+
+            session.beginTransaction();
+
+            int id = 4;
+
+            InstructorDetail instructorDetail = session.get(InstructorDetail.class, id);
+
+
+            System.out.println("Found instructor Detail: " + instructorDetail);
+
+            System.out.println("Found instructor : " + instructorDetail.getInstructor());
+
+
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+
+    }
+}
